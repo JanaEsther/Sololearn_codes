c121617e5562bd6c2d6685dbd3b8460b45582f68
JanaEsther <j.obendraufova@h2o-logic.cz>
2024-03-10 17:00:24 +0100

    adding_methods

---
+++ raising_exceptions.py
@@ -0,0 +1,26 @@
+#Raising Exceptions
+#You are making a program to post tweets. Each tweet must not exceed 42 characters.
+#Complete the program to raise an exception, in case the length of the tweet is more than 42 characters.
+
+#You can use the len() function to calculate the length of the string.
+
+
+tweet = input()
+
+try:
+    if len(tweet) > 42:
+      raise ValueError("Tweet cannot exceed 42 characters")
+except:
+    print("Error")
+else:
+    print("Posted")
+
+
+
+
+
+
+
+
+
+
