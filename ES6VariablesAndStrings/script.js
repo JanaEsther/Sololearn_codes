4a9958980a38f5a68aa8dd9d7e088f96165b4ca6
JanaEsther <j.obendraufova@h2o-logic.cz>
2024-03-30 12:38:16 +0100

    ES6Variables

---
+++ ES6VariablesAndStrings/script.js
@@ -0,0 +1,10 @@
+function main() {
+    var country = readLine();
+    var capital = readLine();
+
+    console.log(countryCard(country, capital)){
+        //cmoplete the function
+        //use backtick (``) for template literal
+        return `Name: ${country}, Capital: ${capital}`;
+    }
+}
